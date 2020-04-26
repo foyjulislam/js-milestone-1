fe716ec19127702988c8bde95d070482c387a05d
foyjulislam <foyjulislam.syl20@gmail.com>
2020-04-27 00:25:44 +0600

    js String charAt

--- JsString.js
+++ JsString.js
@@ -1,8 +1,22 @@
 // charAt() method
 
+// ##step-01
+
 const sentence = 'The quick brown fox jumps over the lazy dog.';
 //const index = 6;
 const index = 13;
 console.log(
   'The character at index ' + index + ' is ' + sentence.charAt(index)
+);
+
+// ##step-02
+
+const indexNumber = 'why use js charAt method ?';
+const indexIs = 25;
+
+console.log(
+  'The Character at index number ' +
+    indexIs +
+    ' is ' +
+    indexNumber.charAt(indexIs)
 );
