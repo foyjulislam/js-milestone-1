30bae159b360f41c7ff8a1ba7e836c521199b49f
foyjulislam <foyjulislam.syl20@gmail.com>
2020-04-27 00:08:06 +0600

    charAt practice

---
+++ JsString.js
@@ -0,0 +1,8 @@
+// charAt() method
+
+const sentence = 'The quick brown fox jumps over the lazy dog.';
+//const index = 6;
+const index = 13;
+console.log(
+  'The character at index ' + index + ' is ' + sentence.charAt(index)
+);
